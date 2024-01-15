1f7d43a203fed11abb9fde7a83fee35c6c25c5d5
MilosNikolic131 <milosnikolic99x@gmail.com>
2024-01-15 20:31:30 +0100

    home, navbar

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1></h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/flights">Flights</a>
+                <a href="/reservations">Reservations</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
